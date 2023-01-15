478bc34888e1f8a6c98a4eeab98eb2b8aeaf3ac9
Betty <betty@Bettys-MacBook-Air.local>
2023-01-15 13:58:14 -0600

    added how to add product to cart

--- src/components/cart-dropdown/cart-dropdown.jsx
+++ src/components/cart-dropdown/cart-dropdown.jsx
@@ -1,10 +1,12 @@
 import './cart-dropdown.styles.scss';
 import Button from '../button/button';
+import CartItem from '../cart-item/cart-item';
 
 const CartDropdown = () =>{
     return(
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
+                {[].map(item => <CartItem cartItem={item}/>)}
             </div>
             <Button> Go to checkout</Button>
         </div>
