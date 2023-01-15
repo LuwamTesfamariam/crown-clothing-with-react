99755f4bbb07ea7da78a1ed592bd12d415b5f0ba
Betty <betty@Bettys-MacBook-Air.local>
2023-01-15 14:02:07 -0600

    u

--- src/context/cartContext.jsx
+++ src/context/cartContext.jsx
@@ -1,7 +1,19 @@
 import { createContext, useState } from "react";
 
 const addCartItem = (cartItems, productToAdd) => {
+    
+    const existingCartItem = cartItems.find(
+        (cartItem) => cartItem.id === productToAdd.id
+    );
 
+    if (existingCartItem) {
+        return cartItems.map((cartItem) =>
+            cartItem.id === productToAdd.id
+                ? { ...cartItem, quantity: cartItem.quantity + 1 }
+                : cartItem
+        );
+    }
+    return [...cartItems, { ...productToAdd, quantity: 1 }];
 }
 
 export const CartContext = createContext({
